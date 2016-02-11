39b917c07582eb0dd03a598dea047eedd04bf04d
Liad Yosef <liad.yosef@gmail.com>
2016-02-11 02:25:23 +0200

    add high order pure wrapper

---
+++ src/high-order.js
@@ -0,0 +1,34 @@
+import shallowEqual from './shallowEqual';
+
+export default function pureStateless(statelessComponent) {
+
+    let lastProps = undefined;
+    let lastContext = undefined;
+
+    let savedResult = undefined;
+    let savedException = undefined;
+
+    return function (props, context) {
+        let shouldUpdate = !shallowEqual(props, lastProps) || !shallowEqual(context, lastContext);
+
+        if (shouldUpdate) {
+          lastProps = props;
+          lastContext = context;
+
+          savedResult = undefined;
+          savedException = undefined;
+
+          try {
+            savedResult = statelessComponent.apply(this, arguments);
+          } catch(e) {
+            savedException = e;
+          }
+        }
+
+        if (savedException !== undefined) {
+          throw (savedException);
+        }
+
+        return savedResult;
+    }
+}
